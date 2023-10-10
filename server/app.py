f7394d99ec199c14c905ecf4683c1ce80ba48be5
TracyAntonia <aantonjuguna@gmail.com>
2023-10-11 02:26:55 +0300

    Still in progress

--- server/app.py
+++ server/app.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
-app = Flask(__name__)
+app = Flask(__name)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/')
@@ -19,16 +15,29 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        return f'Name: {animal.name}<br>Species: {animal.species}<br>Zookeeper: {animal.zookeeper.name}<br>Enclosure: {animal.enclosure.environment}'
+    else:
+        return 'Animal not found'
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        animals = [animal.name for animal in zookeeper.animals.all()]
+        return f'Name: {zookeeper.name}<br>Birthday: {zookeeper.birthday}<br>Animals: {", ".join(animals)}'
+    else:
+        return 'Zookeeper not found'
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        animals = [animal.name for animal in enclosure.animals.all()]
+        return f'Environment: {enclosure.environment}<br>Open to Visitors: {enclosure.open_to_visitors}<br>Animals: {", ".join(animals)}'
+    else:
+        return 'Enclosure not found'
 
-
-if __name__ == '__main__':
+if __name__ == '__main':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,24 +1,17 @@
-#!/usr/bin/env python3
-
 from random import choice as rc
-
 from faker import Faker
-
-from app import app
-from models import db, Zookeeper, Animal, Enclosure
+from app import app, db, Zookeeper, Animal, Enclosure
 
 fake = Faker()
 
 with app.app_context():
-
     Animal.query.delete()
     Zookeeper.query.delete()
     Enclosure.query.delete()
 
     zookeepers = []
     for n in range(25):
-        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
-            start_date='-70y', end_date='-18y'))
+        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(start_date='-70y', end_date='-18y'))
         zookeepers.append(zk)
 
     db.session.add_all(zookeepers)
@@ -39,7 +32,7 @@ with app.app_context():
     for n in range(200):
         name = fake.first_name()
         while name in [a.name for a in animals]:
-            name=fake.first_name()
+            name = fake.first_name()
         a = Animal(name=name, species=rc(species))
         a.zookeeper = rc(zookeepers)
         a.enclosure = rc(enclosures)
@@ -47,4 +40,3 @@ with app.app_context():
 
     db.session.add_all(animals)
     db.session.commit()
-
